93ed2fb5e5ab1cd9041901045dc255b1f4fe2165
Joachim <joact@live.dk>
2024-12-05 15:15:21 +0100

    tes2l

--- public/js/login.js
+++ public/js/login.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () => {
         }
 
         try {
-            const response = await fetch("http://localhost:1000/api/login", {
+            const response = await fetch("http://165.227.134.194/api/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
